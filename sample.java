48eb43bfee094954431a405119437e2e2daf8bb6
Madhushree2303 <91212488+Madhushree2303@users.noreply.github.com>
2021-10-15 13:09:54 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,7 +3,6 @@ public class newclass {
 	public static void main(String[] args) {
 		System.out.println("hello world 1");
 		System.out.println("hello world 2");
-		System.out.println("hello world 3");
 		System.out.println("hello world 4");
 		System.out.println("hello world 5678910");
 
